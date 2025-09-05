5e72efba1c980c6fdafb6ae00b95623d6cf91193
Emerson Paduan <emerson@paduan.dev.br>
2025-09-04 23:05:44 -0300

    aula 02

---
+++ aula01/Exemplo01.java
@@ -0,0 +1,30 @@
+
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        System.out.println("Boa noite!");
+
+        // tipos primitivos? int double char boolean
+        int numero = 10;
+        // tipos não primitivos: Integer, Double Character, 
+        Integer numero2 = 12; 
+
+        // Integer n = new Integer(12);
+        String nome = "Nome";
+        
+        // if (true) {
+            
+        // } else {
+            
+        // }
+
+        // while (true) {
+            
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+            
+        // }
+    }
+}
+
